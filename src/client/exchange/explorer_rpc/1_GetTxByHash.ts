df6012581c175e0a24d64d58ddeddcaedbeb8c8f
Achilleas Kalantzis <kalantzis.achilleas@gmail.com>
2022-05-03 18:41:18 +0300

    feat: add ExplorerRPC

---
+++ src/client/exchange/explorer_rpc/1_GetTxByHash.ts
@@ -0,0 +1,16 @@
+import { getNetworkInfo, Network } from "@injectivelabs/networks";
+import { ExchangeClient } from "@injectivelabs/sdk-ts";
+import { protoObjectToJson } from "@injectivelabs/sdk-ts";
+
+(async () => {
+  const network = getNetworkInfo(Network.Testnet);
+  const tx_hash =
+    "d7a1c7ee985f807bf6bc06de728810fd52d85141549af0540486faf5e7de0d1d";
+
+  const exchangeClient = new ExchangeClient.ExchangeGrpcClient(
+    network.exchangeApi
+  );
+  const tx = await exchangeClient.explorerApi.fetchTxByHash(tx_hash);
+
+  console.log(protoObjectToJson(tx, {}));
+})();
