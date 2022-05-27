226aa08756f64ba1951329a5f7a0f29f1012eefc
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2022-05-27 15:46:23 +0200

    chore: update packages

--- src/client/exchange/derivative_exchange_rpc/2_Markets.ts
+++ src/client/exchange/derivative_exchange_rpc/2_Markets.ts
@@ -1,23 +1,17 @@
 import { getNetworkInfo, Network } from "@injectivelabs/networks";
 import { protoObjectToJson, ExchangeClient } from "@injectivelabs/sdk-ts";
 
-
 (async () => {
   const network = getNetworkInfo(Network.Testnet);
 
   const marketStatus = "active";
   const quoteDenom = "peggy0xdAC17F958D2ee523a2206206994597C13D831ec7";
 
-  const exchangeClient = new ExchangeClient.ExchangeGrpcClient(
-    network.exchangeApi
-  );
+  const exchangeClient = new ExchangeClient.GrpcClient(network.exchangeApi);
+  const markets = await exchangeClient.derivatives.fetchMarkets({
+    marketStatus: marketStatus,
+    quoteDenom: quoteDenom,
+  });
 
-  const markets = await exchangeClient.derivativesApi.fetchDerivativeMarkets(
-    {
-      marketStatus: marketStatus,
-      quoteDenom: quoteDenom
-    }
-  );
-
-  console.log(protoObjectToJson(markets, {}));
+  console.log(protoObjectToJson(markets));
 })();
